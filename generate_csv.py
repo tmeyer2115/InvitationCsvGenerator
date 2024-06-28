5289d694f293689184c77fbab36f7b2f1788e3d4
Thomas Meyer <tmeyer@yext.com>
2024-06-28 11:27:33 -0400

    Second commit.

--- generate_csv.py
+++ generate_csv.py
@@ -1,26 +1,33 @@
 import csv
+import random
 
-def generate_dummy_csv(filename):
+def generate_dummy_csv(filename, numEntityIds, numInvites):
     # Define column headers
     fields = ['entityId', 'firstName', 'lastName', 'contact']
     
     # Dummy data
-    entityId = '4'
     contact = 'bmarterella'
+    entityIds = select_random_entity_ids(filename, numEntityIds)
     
     # Generate data for 1000 rows
     rows = []
-    for i in range(6000):
-        row = [entityId, 'DummyFirstName', 'DummyLastName', "NEWmseverance+%s" %i +"devblah@yext.com"]
+    for i in range(numInvites):
+        row = [random.choice(entityIds), 'DummyFirstName', 'DummyLastName', "NEWmseverance+%s" %i +"devblah@yext.com"]
         rows.append(row)
     
     # Write to CSV file
-    with open(filename, 'w') as csvfile:
+    with open('invites.csv', 'w') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(fields)
         csvwriter.writerows(rows)
 
+def select_random_entity_ids(filename, numEntityIds):
+    with open(filename) as csvfile:
+        entityIdRows = csv.reader(csvfile)
+        entityIds = [entityIdRow[0] for entityIdRow in entityIdRows]
+        return random.sample(entityIds, numEntityIds)
+        
 if __name__ == "__main__":
-    filename = 'dummy_data.csv'
-    generate_dummy_csv(filename)
+    filename = 'qa_entity_ids.csv'
+    generate_dummy_csv(filename, 1000, 1000)
     print("DONE!")
